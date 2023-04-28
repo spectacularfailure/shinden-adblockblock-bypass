9dd536a667cc788220290e4344eac1211b32e2f7
jsmith <a@a333.a>
2023-04-28 21:35:26 +0200

    feat(shinden): intial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        Shinden AdBlock blocker bypass
+// @include      /^https?://shinden.pl/(episode|epek)/*/
+// @grant       none
+// @require     https://code.jquery.com/jquery-3.6.4.min.js
+// @version     1.1
+// @author      @spectacularfailure & @nextu1337
+// @description Naprawiona wersja skryptu bypassującego adblock blocka do Shindena
+// ==/UserScript==
+
+const apiUrl = "https://api4.shinden.pl"
+
+function loadPlayer(playerId) {
+  const auth = _Storage.basic
+  $.ajax({
+    url: `${apiUrl}/xhr/${playerId}/player_load?auth=${auth}`,
+    xhrFields: {
+      withCredentials: true
+    }
+  })
+
+  setTimeout(() => {
+    $.ajax({
+      url: `${apiUrl}/xhr/${playerId}/player_show?auth=${auth}&width=765&height=-1`,
+      xhrFields: {
+        withCredentials: true
+      },
+      success: (data) => {
+          $('#player-block').html(data);
+      }
+    })
+  }, 6000)
+}
+
+$(".change-video-player").on("click", event => {
+  $('#player-block').html("<label>Proszę poczekać 6 sekund....</label>");
+
+  dataEpisode = JSON.parse($(event.target).attr("data-episode"))
+
+  loadPlayer(dataEpisode["online_id"])
+})
+
